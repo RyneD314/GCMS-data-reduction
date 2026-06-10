14adc995925b05311955508957704958e2ffd987
Ryne Dingler <85257577+RyneD314@users.noreply.github.com>
2026-06-10 18:42:38 -0500

    Example Shell files

---
+++ gcms_3dith.sh
@@ -0,0 +1,3 @@
+source gcms_ifu_env/bin/activate
+
+python gcms_reduce.py --rawdir raw_data --outdir output --flat flat.fits --arc arc.fits --calib fiber_positions.csv
---
+++ gcms_6dith.sh
@@ -0,0 +1,8 @@
+python gcms_reduce_6dither.py \
+    --rawdir /path/to/raw \
+    --outdir /path/to/output \
+    --flat flat.fits \
+    --arc arc.fits \
+    --calib fiber_positions.csv \
+    --patternA_offsets 0 0 2 0 1 2 \
+    --patternB_offsets 1 1 3 1 2 3
