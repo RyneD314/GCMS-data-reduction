7a68c8a795dbb745af3d3c389bf29b48b596d944
Ryne Dingler <85257577+RyneD314@users.noreply.github.com>
2026-06-10 23:38:26 -0500

    Update gcms_3dith.sh

--- gcms_3dith.sh
+++ gcms_3dith.sh
@@ -1,3 +1,18 @@
-source gcms_ifu_env/bin/activate
+#!/bin/bash
+# 3-dither reduction with the unified pipeline
 
-python gcms_reduce.py --rawdir raw_data --outdir output --flat flat.fits --arc arc.fits --calib fiber_positions.csv
+python gcms_reduce_unified.py \
+    --sci_dir raw_data/science_3dith \
+    --bias_dir raw_data/biases \
+    --flat_dir raw_data/flats \
+    --arc_dir raw_data/arcs \
+    --sky_dir raw_data/skies \
+    --n_dither 3 \
+    --offsets "0,0;2,0;1,2" \
+    --fiber_coords fiber_positions.csv \
+    --out_cube gcms_3dith_cube.fits \
+    --object_name "NGC 1277" \
+    --pixscale 1.0 \
+    --arc_lines arc_lines_auto.txt \
+    --use_sky_zero \
+    --z 0.0172
