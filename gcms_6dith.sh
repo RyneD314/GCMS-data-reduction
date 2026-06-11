8d13995f57ef0aee492975674c60756805ac12c9
Ryne Dingler <85257577+RyneD314@users.noreply.github.com>
2026-06-10 23:38:44 -0500

    Update gcms_6dith.sh

--- gcms_6dith.sh
+++ gcms_6dith.sh
@@ -1,8 +1,18 @@
-python gcms_reduce_6dither.py \
-    --rawdir /path/to/raw \
-    --outdir /path/to/output \
-    --flat flat.fits \
-    --arc arc.fits \
-    --calib fiber_positions.csv \
-    --patternA_offsets 0 0 2 0 1 2 \
-    --patternB_offsets 1 1 3 1 2 3
+#!/bin/bash
+# 6-dither reduction with the unified pipeline
+
+python gcms_reduce_unified.py \
+    --sci_dir raw_data/science_6dith \
+    --bias_dir raw_data/biases \
+    --flat_dir raw_data/flats \
+    --arc_dir raw_data/arcs \
+    --sky_dir raw_data/skies \
+    --n_dither 6 \
+    --offsets "0,0;2,0;1,2;1,1;3,1;2,3" \
+    --fiber_coords fiber_positions.csv \
+    --out_cube gcms_6dith_cube.fits \
+    --object_name "NGC 1277" \
+    --pixscale 1.0 \
+    --coeff_file wavelength_coeffs.txt \
+    --use_sky_zero \
+    --z 0.0172
